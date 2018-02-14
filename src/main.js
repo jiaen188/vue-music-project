a276cea3e5c912a5863030d91bc2fcdf851be34d
jiaen188 <244189981@qq.com>
2018-02-14 16:33:59 +0800

    complete basic of project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+fastclick.attach(document.body) // 解决移动端快速点击问题，同时没有300ms的延迟
 
 /* eslint-disable no-new */
 new Vue({
