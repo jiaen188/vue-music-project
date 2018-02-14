3f7d100f6cf2a3f011edaadd582495c0a32f2cb8
jiaen188 <244189981@qq.com>
2018-02-14 15:32:09 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+import  "~common/style/variable"
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app
+  color: $color-theme
+</style>
