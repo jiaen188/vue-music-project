a1af673b47aaad72167de737352bc71bb28145e5
jiaen188 <244189981@qq.com>
2018-02-15 15:19:58 +0800

    轮播图完成

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -5,6 +5,7 @@
       </slot>
     </div>
     <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :class="{active: currentPageIndex === index}"></span>
     </div>
   </div>
 </template>
@@ -14,6 +15,12 @@ import BScroll from 'better-scroll'
 import {addClass} from 'common/js/dom'
 
 export default {
+  data () {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
   props: { // 配置轮播的属性： 是否循环，是否自动播放，延迟时间
     loop: {
       type: Boolean,
@@ -31,11 +38,21 @@ export default {
   mounted () {
     setTimeout(() => {
       this._setSliderWidth()
+      this._initDots()
       this._initSlider()
+      if (this.autoPlay) this._play()
     }, 20)
+
+    // 手动改变页面宽度的时候，需要重新计算slider的width
+    window.addEventListener('resize', () => {
+      // 如果slider还没有渲染完成，不重新执行计算高度
+      if (!this.slider) return
+      this._setSliderWidth(true)
+      this.slider.refresh()
+    })
   },
   methods: {
-    _setSliderWidth () {
+    _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
 
       let width = 0
@@ -48,10 +65,15 @@ export default {
         width += sliderWidth
       }
 
-      if (this.loop) {
+      // 如果是 视口改变引起的重新计算宽度， 是不需要再加两个子元素宽度的
+      if (this.loop && !isResize) {
         width += 2 * sliderWidth
       }
       this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    // 初始化 圆点
+    _initDots () {
+      this.dots = new Array(this.children.length)
     },
     _initSlider () {
       this.slider = new BScroll(this.$refs.slider, {
@@ -63,7 +85,34 @@ export default {
         snapThreshold: 0.3,
         snapSpeed: 400
       })
+
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        if (this.loop) {
+          pageIndex -= 1
+        }
+        this.currentPageIndex = pageIndex
+
+        if (this.autoPlay) {
+          // 清空是为了防止人拖动后，自动滚动的效果受到影响
+          clearTimeout(this.timer)
+          this._play()
+        }
+      })
+    },
+    _play () {
+      let pageIndex = this.currentPageIndex + 1
+      if (this.loop) pageIndex += 1
+
+      this.timer = setTimeout(() => {
+        // 第一个参数，表示滚动到哪一个页面，第二个参数，表示水平方向滚动，第三个参数表示滚动时间
+        this.slider.goToPage(pageIndex, 0, 400)
+      }, this.interval)
     }
+  },
+  destroyed () {
+    // 当从recommend页面切换到其他页面的时候， 清空计时器
+    clearTimeout(this.timer)
   }
 }
 </script>
