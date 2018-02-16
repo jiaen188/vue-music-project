3fb164fb5b60c9f69fcea71950ac00a14f7e2e89
jiaen188 <244189981@qq.com>
2018-02-16 22:44:52 +0800

    歌手页面右侧快速导航栏完成

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,7 +1,12 @@
 <template>
-  <scroll class="listview" :data="data">
+  <scroll ref="listview" 
+          class="listview" 
+          :data="data" 
+          :listenScroll="listenScroll"
+          :probeType="probeType"
+          @scroll="scroll">
     <ul>
-      <li v-for="group in data" class="list-group">
+      <li ref="listGroup" v-for="group in data" class="list-group">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="item in group.items" class="list-group-item">
@@ -11,17 +16,126 @@
         </ul> 
       </li>
     </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" 
+            class="item" 
+            :data-index="index" 
+            :class="{'current': currentIndex === index}">
+          {{item}}
+        </li>
+      </ul>
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
+import { getData } from 'common/js/dom'
+const ANFHOR_HEIGHT = 18
 
 export default {
+  created () {
+    // 用来保存 touchstart的pageY和touchMove的pageY，然后除行高18， 取整后，就是移动了几个字母距离
+    this.touch = {}
+    this.listenScroll = true
+    this.listHeight = []
+    // probeType为3是表示监听快速滚动动画
+    this.probeType = 3
+  },
+  data () {
+    return {
+      scrollY: -1,
+      currentIndex: 0
+    }
+  },
   props: {
     data: {
       type: Array,
       default: []
+    }
+  },
+  computed: {
+    shortcutList () {
+      return this.data.map(group => {
+        return group.title.substr(0, 1)
+      })
+    }
+  },
+  methods: {
+    onShortcutTouchStart (e) {
+      let anchorIndex = getData(e.target, 'index')
+      let firstTouch = e.touches[0]
+      this.touch.y1 = firstTouch.pageY
+      this.touch.anchorIndex = anchorIndex
+      // this.$refs.listview.scrollToElement(this.$refs.listGroup[anchorIndex], 0)
+      this._scrollTo(anchorIndex)
+    },
+    onShortcutTouchMove (e) {
+      let firstTouch = e.touches[0]
+      this.touch.y2 = firstTouch.pageY
+      // | 0 是为了取整
+      let delta = (this.touch.y2 - this.touch.y1) / ANFHOR_HEIGHT | 0
+      // 获得的anchorIndex是字符串，所以要先parseInt
+      let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+      this._scrollTo(anchorIndex)
+    },
+    // scroll组件emit出来的scroll方法被html中绑定到scroll方法上
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    _scrollTo (index) {
+      // 侧边栏上下各有一个块黑块， 是为了美观，实际上是没有点击效果
+      if (!index && index !== 0) return
+      // 边界处理，如果已经拖动超出嘴上，或者最下边
+      if (index < 0) {
+        index = 0
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight.length - 2
+      }
+      // 如果是直接点击右侧的时候，因为没有触发emit的scroll， 所以需要我们手动将scrollY赋值
+      this.scrollY = -this.listHeight[index]
+      // 第一个参数是要滚动到 第index个子元素， 第二个参数是，是否需要缓动动画
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+    },
+    // 计算每个 姓名歌手栏目的 高度, 并放入listHeight数组中
+    _calculateHeight () {
+      this.listHeight = []
+      const list = this.$refs.listGroup
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < list.length; i++) {
+        let item = list[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    }
+  },
+  watch: {
+    data() {
+      setTimeout(() => {
+        this._calculateHeight()
+      }, 20)
+    },
+    scrollY (newY) {
+      const listHeight = this.listHeight
+      // 当滚动到顶部，newY > 0
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      }
+      // 当滚动到中间部分
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i]
+        let height2 = listHeight[i + 1]
+        // 如果没有下限（那就是最后一个） 或者在上下限之间，就是i
+        if (-newY >= height1 && -newY < height2) {
+          this.currentIndex = i
+          return
+        }
+      }
+      // 当滚动到底部， 且-newY大于最后一个元素的上限
+      this.currentIndex = listHeight.length - 2
     }
   },
   components: {
--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -8,6 +8,7 @@ import BScroll from 'better-scroll'
 
 export default {
   props: {
+    // probeType为1是表示缓动动画
     probeType: {
       type: Number,
       default: 1
@@ -19,6 +20,11 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    // 是否需要监听滚动， 默认不需要， 如果需要要在初始化中emit一个pos
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted () {
@@ -34,6 +40,13 @@ export default {
           probeType: this.probeType,
           click: this.click
       })
+
+      if (this.listenScroll) {
+        let me = this
+        this.scroll.on('scroll', pos => {
+          me.$emit('scroll', pos)
+        })
+      }
     },
     enable () {
       this.scroll && this.scroll.enable()
@@ -43,6 +56,12 @@ export default {
     },
     refresh () {
       this.scroll && this.scroll.refresh()
+    },
+    scrollTo () {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement () {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   },
   watch: {
